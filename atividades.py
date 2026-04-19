0818f09aa84ff439a03f6153b502216e64700140
gabrielseisl <gabrielsseislppp@gmail.com>
2026-04-19 11:01:21 -0300

    atividade.py

--- atividades.py
+++ atividades.py
@@ -1,9 +1,9 @@
-         atividade 1
+atividade 1
          
 nome = input("qual seu nome: ")
 idade = int(input("Qual sua idade: "))
 cidade = input("Qual sua cidade: ")
-print(f"Olá,meu nome é {nome}) tenho{idade}anos e moro em {cidade}
+print(f"Olá,meu nome é {nome} tenho {idade}anos e moro em {cidade}")
 
                 atividade 2
 n1 = float(input("digite um numero: "))
@@ -19,7 +19,6 @@ n1=int(input("digite um numero: "))
 
 for i  in range (n1,n1*10,n1):
     print(i)
-
                 atividade 4
 numero=""
 
@@ -34,21 +33,31 @@ while numero != "23":
         break
 
             atividade 5
-compras=[]
+compras = []
 
-c1=(input("digite uma compra: "))
-c2=(input("digite uma compra: "))
-c3=(input("digite uma compra: "))
-c4=(input("digite uma compra: "))
-c5=(input("digite uma compra: "))
+c1 = input("Digite uma compra: ")
+c2 = input("Digite uma compra: ")
+c3 = input("Digite uma compra: ")
+c4 = input("Digite uma compra: ")
+c5 = input("Digite uma compra: ")
 
-compras=compras(enumerate[c1,c2,c3,c4,c5])
-print(compras)
 
-c6=(input("alguma nova compra?:"))
+compras = [c1, c2, c3, c4, c5]
 
-compras.append(["6 " + c6])
-print(compras)
+
+print("lista de compras:")
+for i, item in enumerate(compras, start=1):
+    print(f"{i}. {item}")
+
+c6 = input("Alguma nova compra? ")
+
+compras.append(c6)
+
+
+print("lista atualizada:")
+for i, item in enumerate(compras,start=1):
+    print(f"{i}. {item}")
+
 
         atividade 7
 
@@ -80,7 +89,7 @@ par_ou_impar()
     
 calcular_imc()
 
-            atividade 9
+            
 
 def calcular_imc():
     peso=int(input("qual seu peso: "))
@@ -102,7 +111,7 @@ def calcular_imc():
 def classificar_imc():
     print("qual seu imc?")
     classificacao=int(input("digite aqui seu imc: "))
-    if  (classificacao<18,5 or classificacao>25):
+    if  (classificacao<18.5 or classificacao>25):
         print("imc ruim")
     else:
         print("imc bom")
@@ -120,3 +129,60 @@ while True:
         classificar_imc()
     elif escolha == "3":
          break
+
+                atividade 9
+
+tarefas = []
+
+def listar():
+    if len(tarefas) == 0:
+        print('Nenhuma tarefa cadastrada.')
+    else:
+        for i, t in enumerate(tarefas, 1):
+            print(f'{i}. {t}')
+
+def adicionar():
+    print("Adicionar tarefas")
+    t1=(input("Tarefa Nova: "))
+    t2=(input("Tarefa Nova:  "))
+    tarefas.append([t1,t2])
+    
+
+
+
+def remover(tarefas):
+    if tarefas=="":
+        print("Nenhuma tarefa para remover.")
+        return tarefas
+
+    print("\nTarefas:")
+    for i, tarefa in enumerate(tarefas):
+        print(f"{i+1} - {tarefa}")
+
+    escolha = int(input("Digite o número da tarefa que deseja remover: "))
+    indice = escolha - 1
+
+    if 0 <= indice < len(tarefas):
+        print(f"Tarefa '{tarefas[indice]}' removida com sucesso!")
+        del tarefas[indice]
+    else:
+        print("Número inválido.")
+
+    return tarefas
+        
+
+while True:
+    print('\n1 - Adicionar 2 - Remover 3 - Listar 4 - Sair')
+    
+    opcao = input('Escolha: ')
+
+    if opcao == "3":
+        listar()
+    elif opcao == "1":
+        adicionar()
+    elif opcao == "2":
+        remover(tarefas)
+    elif opcao == "4":
+        print("Saindo")
+        break
+    
